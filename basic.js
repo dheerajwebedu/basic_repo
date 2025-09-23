83ee67636d3f37bf9431c54320d93a2520116ffd
dheerajwebedu <dheerajsharma16g@gmail.com>
2025-09-23 07:15:01 +0530

    added basic file

---
+++ basic.js
@@ -0,0 +1,36 @@
+let a = 100;
+console.log(a)
+
+let b = 50;
+console.log(b)
+
+let c = 150
+console.log("a", c)
+
+let d = 200;
+console.log(d)
+
+
+// Oprations
+// 1. Addition(+)
+console.log(a + b)
+
+
+//2. Substraction(-)
+console.log(a - b)
+
+//3. Multiplication(*)
+console.log(a * b)
+
+//4. Division(/)
+console.log(a / b)
+
+//5.Power(Exponenatiation)--(**)
+console.log(50 ** 2)
+
+//6. Modulous (reminder)(%)
+console.log(10 % 3)
+
+//7. Increment(++) = +1
+console.log(++a) //a = 101
+console.log(--b)
